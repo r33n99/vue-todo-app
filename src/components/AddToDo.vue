2a8789d902f543837f72c2fcfd83d99bc38e6a5c
Reen666 <rinni499@gmail.com>
2021-03-15 16:34:47 +0600

    Added todolist

---
+++ src/components/AddToDo.vue
@@ -0,0 +1,41 @@
+<template>
+    <form @submit.prevent="onSubmit">
+        <input type="text" v-model="title" >
+        <button type="submit">Create</button>
+    </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: ''
+        }
+    },
+    methods: {
+        onSubmit() {
+            console.log('Submit', this.title)
+            if(this.title.trim()) {
+                const newToDo = {
+                    id: Date.now(),
+                    title:this.title,
+                    completed: false
+                }
+            this.$emit('add-todo', newToDo)
+
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+form {
+    display: flex;
+
+}
+
+input {
+    width: 500px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h2>Home page</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam animi nobis repellendus dicta repellat quia facere, nostrum unde rem ipsa mollitia quasi veniam porro nulla. Aliquam harum dolores maxime libero.</p>
+
+        <router-link to="/todos">Go to second page</router-link>
+    </div>
+</template>
